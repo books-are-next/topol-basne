1dea2935b67e724b0e883414353cd0bc4f3e545f
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-08-01 13:59:29 +0000

    Deploying to docs from @ books-are-next/topol-basne@9e3e3a6134ef25f1608e4fb2c6ed57f7d72d2b20 🚀

--- epub2nb-output/content/_book.md
+++ epub2nb-output/content/_book.md
@@ -13,6 +13,7 @@ meta:
   edition: poezie
   publisherShort: mkp
   contentType: poetry
+  isbn: 978-80-274-2381-1
 readingOrder:
   - promo.html
   - basne_005.html
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-503980c';
+const CACHE = 'cache-9e3e3a6';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
